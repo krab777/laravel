3c5ff32518a30d031d28ea0068762c28dadf72ef
crab777 <baiborodin00@gmail.com>
2020-03-19 00:55:07 +0200

    homeWork_1

--- resources/views/first_page.blade.php
+++ resources/views/first_page.blade.php
@@ -65,33 +65,9 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel1
-                </div>
-
-                <div class="links">
-                    <a href="http://localhost/blog/public/user/1">User 1</a>
-                    <a href="http://localhost/blog/public/user/2">User 2</a>
-                    <a href="http://localhost/blog/public/first_page_view">Page view</a>
-                    <a href="http://localhost/blog/public/first_page">Page without view</a>
-
-
-                   
+                    {{$hello ?? 'Hello'}}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+	// $data = request()
     return view('welcome');
 });
+
+
+Route::get('/first_page_view', function () {
+	$hello = request('hello');
+
+    return view('first_page', compact('hello'));
+});
+
+Route::get('/first_page', function () {
+    return 'first_page';
+});
+
+Route::get('user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+Route::get('users/{name?}', function ($name = 'Usual Name') {
+    return $name;
+});
+
+
